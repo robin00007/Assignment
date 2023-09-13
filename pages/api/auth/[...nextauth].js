7c3b8f5d34a4f232ef84c21df5e4199f908bc4d0
robin00007 <robin_20220@aitpune.edu.in>
2023-09-14 00:02:01 +0530

    next secret added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,28 +1,26 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GoogleProvider({
-        clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-        clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ,
-        authorization: {
-            params: {
-              prompt: "consent",
-              access_type: "offline",
-              response_type: "code"
-            }
-          },
-        callback:{
-          // async redirect({ url, baseUrl }) {
-          //   console.log("robin",baseUrl+'/dashboard')
-          //   return baseUrl+'/dashboard'
-          // },
-        }
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
 
-    })
-
-    
-    
+      callback: {
+        // async redirect({ url, baseUrl }) {
+        //   console.log("robin",baseUrl+'/dashboard')
+        //   return baseUrl+'/dashboard'
+        // },
+      },
+      secret: process.env.NEXTAUTH_SECRET,
+    }),
   ],
-}
-export default NextAuth(authOptions)
+};
+export default NextAuth(authOptions);
